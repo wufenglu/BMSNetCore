3adda3d69da985eb674ee7102d3f6a6d3a51a34b
wufenglu <871339677@qq.com>
2019-08-28 10:04:01 +0800

    BMS\00.Platform\YK.Platform.Core\Helper\Emit.cs

---
+++ BMS/00.Platform/YK.Platform.Core/Helper/Emit.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YK.Platform.Core.Helper
+{
+    /// <summary>
+    /// Emit
+    /// </summary>
+    public class Emit
+    {
+        /// <summary>
+        /// Emit获取对象的属性值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        public static Func<T, object> EmitGetter<T>(string propertyName)
+        {
+            var type = typeof(T);
+
+            var dynamicMethod = new DynamicMethod("get_" + propertyName, typeof(object), new[] { type }, type);
+            var iLGenerator = dynamicMethod.GetILGenerator();
+            iLGenerator.Emit(OpCodes.Ldarg_0);
+
+            var property = type.GetProperty(propertyName);
+            iLGenerator.Emit(OpCodes.Callvirt, property.GetMethod);
+
+            if (property.PropertyType.IsValueType)
+            {
+                // 如果是值类型，装箱
+                iLGenerator.Emit(OpCodes.Box, property.PropertyType);
+            }
+            else
+            {
+                // 如果是引用类型，转换
+                iLGenerator.Emit(OpCodes.Castclass, property.PropertyType);
+            }
+
+            iLGenerator.Emit(OpCodes.Ret);
+
+            return dynamicMethod.CreateDelegate(typeof(Func<T, object>)) as Func<T, object>;
+        }
+
+        /// <summary>
+        /// Emit设置对象属性值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        public static Action<T, object> EmitSetter<T>(string propertyName)
+        {
+            var type = typeof(T);
+            var dynamicMethod = new DynamicMethod("EmitCallable", null, new[] { type, typeof(object) }, type.Module);
+            var iLGenerator = dynamicMethod.GetILGenerator();
+
+            var callMethod = type.GetMethod("set_" + propertyName, BindingFlags.Instance | BindingFlags.IgnoreCase | BindingFlags.Public);
+            var parameterInfo = callMethod.GetParameters()[0];
+            var local = iLGenerator.DeclareLocal(parameterInfo.ParameterType, true);            
+            iLGenerator.Emit(OpCodes.Ldarg_1);
+            if (parameterInfo.ParameterType.IsValueType)
+            {
+                // 如果是值类型，拆箱
+                iLGenerator.Emit(OpCodes.Unbox_Any, parameterInfo.ParameterType);
+            }
+            else
+            {
+                // 如果是引用类型，转换
+                iLGenerator.Emit(OpCodes.Castclass, parameterInfo.ParameterType);
+            }
+
+            iLGenerator.Emit(OpCodes.Stloc, local);
+            iLGenerator.Emit(OpCodes.Ldarg_0);
+            iLGenerator.Emit(OpCodes.Ldloc, local);
+
+            iLGenerator.EmitCall(OpCodes.Callvirt, callMethod, null);
+            iLGenerator.Emit(OpCodes.Ret);
+
+            return dynamicMethod.CreateDelegate(typeof(Action<T, object>)) as Action<T, object>;
+        }
+    }
+}
